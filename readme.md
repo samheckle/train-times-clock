d4d425a8596d447a5dd309e3e8a365c2fe2912df
samheckle <samheckle@yahoo.com>
2022-10-18 20:01:47 -0400

    added python script

---
+++ readme.md
@@ -0,0 +1,18 @@
+# resources
+https://tigoe.github.io/Wifi101_examples/
+https://github.com/tigoe/Wifi101_examples/blob/main/ArduinoHttpClient_Examples/HttpClientGetJSON/HttpClientGetJSON.ino
+https://github.com/arduino-libraries/ArduinoHttpClient/blob/master/examples/CustomHeader/CustomHeader.ino
+
+https://api.mta.info/#/HelpDocument
+https://api.mta.info/GTFS.pdf
+
+pip install --upgrade protobuf grpcio-tools
+https://developers.google.com/protocol-buffers/docs/pythontutorial
+
+https://github.com/neoterix/nyc-mta-arrival-notify
+https://nycpulse.herokuapp.com/api
+http://mtaapi.herokuapp.com/stations
+
+# display
+https://www.youtube.com/watch?v=mySnfyyL4i4
+
--- train-times/train-times.ino
+++ train-times/train-times.ino
@@ -1,14 +1,26 @@
 #include <WiFiNINA.h>       // use this for MKR1010 and Nano 33 IoT boards
 #include <ArduinoHttpClient.h>
+#include <Arduino_JSON.h>
+
+// libraries to parse proto file
+#include <snappy.h>
+#include <SnappyProto.h>
+#include <pb.h>
+
 #include "arduino_secrets.h"
 
 WiFiClient netSocket;               // network socket to server
-const char server[] = "www.example.com";  // server name
-String route = "/";              // API route
+WiFiSSLClient wifi;
+const char server[] = "https://api-endpoint.mta.info";  // server name
+String route = "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs";              // API route
 // request timestamp in ms:
 long lastRequest = 0;
 // interval between requests:
-int interval = 10000;
+int interval = 5000;
+
+uint8_t buffer[128];
+size_t message_length;
+bool status;
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,17 +46,47 @@ void loop() {
   if (millis() - lastRequest > interval ) {
 
     Serial.println("making request");
-    HttpClient http(netSocket, server, 80);      // make an HTTP client
+    HttpClient http = HttpClient(wifi, server, 443);      // make an HTTP client
+    http.beginRequest();
     http.get(route);  // make a GET request
+    http.sendHeader("x-api-key", API_KEY);
+    http.endRequest();
 
-    while (http.connected()) {       // while connected to the server,
-      if (http.available()) {        // if there is a response from the server,
-        String result = http.readString();  // read it
-        Serial.print(result);               // and print it
-      }
-    }
-    //  // when there's nothing left to the response,
-    http.stop();                     // close the request
+    // read the status code and body of the response
+    int statusCode = http.responseStatusCode();
+//    String response = http.responseBody();
+    Serial.println(statusCode);
+//    pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
+//    status = pb_decode(&stream, SimpleMessage_fields, &http.responseBody());
+//    Serial.print("Status code: ");
+//    Serial.println(statusCode);
+//    Serial.print("Response: ");
+//    Serial.println(response);
+//
+//    // parse the string into a JSONVar object:
+//    JSONVar myObject = JSON.parse(response);
+//
+//    // if you know the object's keys, you can ask for their values:
+////    Serial.print("temperature:\t");
+////    Serial.println(myObject["temperature"]);
+////    Serial.print("humidity:\t");
+////    Serial.println(myObject["humidity"]);
+//
+//    // myObject.keys() can be used to get an array
+//    // of all the keys in the object
+//    JSONVar keys = myObject.keys();
+//    // read over the object, one key at a time:
+//    for (int i = 0; i < keys.length(); i++) {
+//      JSONVar value = myObject[keys[i]];
+//      // what's the key:
+//      Serial.print(keys[i]);
+//      Serial.print("\t");
+//      // and what's the value of the key:
+//      Serial.print(value);
+//      // and what's the type of the value:
+//      Serial.print("\ttype: ");
+//      Serial.println(JSON.typeof(value));
+//    }
     lastRequest = millis();
   }
 
